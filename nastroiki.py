d15b05bdbf54aafc5cacb640fd20080dee223f7e
Timtimich228 <134204322+Timtimich228@users.noreply.github.com>
2023-08-24 21:59:20 +0600

    Update and rename settings.py to nastroiki.py

---
+++ nastroiki.py
@@ -0,0 +1,21 @@
+import math
+width = 1200
+height = 600
+FOV = math.pi/3
+HALF_FOV = FOV/2
+rays =80
+block_size = 64
+step_angle =FOV/rays
+texture_height = 1200
+texture_width = 1200
+texture_scale = texture_width//block_size
+half_width = width/2
+half_height = height/2
+player_pos = (half_width,half_height)
+distance_to_screen = (width / 2) / math.tan(HALF_FOV)
+scale = width // rays
+max_depth = 600
+wall_color = (170, 170, 170)
+dark_color = (00, 0, 0)
+player_angle = 0
+FPS = 60
