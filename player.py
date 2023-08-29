25e0a42c3a2720e797e08995abec817e1340e5d4
Timtimich228 <134204322+Timtimich228@users.noreply.github.com>
2023-08-29 10:35:36 +0600

    Update player.py

--- player.py
+++ player.py
@@ -7,16 +7,19 @@ class Player():
         self.speed = 2.1
 
     def move(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_DOWN]:
-            self.x += self.speed * math.cos(self.angle)
-            self.y -= self.speed * math.sin(self.angle)
-        if key[pygame.K_UP]:
-            self.x -= self.speed * math.cos(self.angle)
-            self.y += self.speed * math.sin(self.angle)
-        if key[pygame.K_LEFT]:
-            self.angle -= 0.03
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.x += self.speed * cos_a
+            self.y += self.speed * sin_a
+        if keys[pygame.K_DOWN]:
+            self.x += -self.speed * cos_a
+            self.y += -self.speed * sin_a
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
 
-        if key[pygame.K_RIGHT]:
-            self.angle += 0.03
+
 
